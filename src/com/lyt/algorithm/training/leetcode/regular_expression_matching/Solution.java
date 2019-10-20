1938a477472856fa1c0ceb25ef32891a7f25f42f
kaogu <liangytcomputer@163.com>
2019-10-20 18:26:39 +0800

    给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
    '.' 匹配任意单个字符
    '*' 匹配零个或多个前面的那一个元素
    所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
    
    说明:
    
    s 可能为空，且只包含从 a-z 的小写字母。
    p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/regular-expression-matching
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ src/com/lyt/algorithm/training/leetcode/regular_expression_matching/Solution.java
@@ -0,0 +1,86 @@
+package com.lyt.algorithm.training.leetcode.regular_expression_matching;
+
+class Solution {
+    private int[][] resultArray;
+    private char[] str;
+    private char[] paternStr;
+
+    public boolean isMatch(String s, String p) {
+        if (null == p || p.isEmpty()) {
+            return null == s || s.isEmpty();
+        }
+        if (p.charAt(0) == '*') {
+            return false;
+        }
+        paternStr = p.toCharArray();
+        if (null == s || s.isEmpty()) {
+            return isEmptyPatern(paternStr, 0, paternStr.length);
+        }
+        resultArray = new int[s.length() + 1][p.length() + 1];
+        str = s.toCharArray();
+        return match(0, 0, '-');
+    }
+
+    private boolean isEmptyPatern(char[] paternStr, int beginIndex, int endIndex) {
+        boolean empty = true;
+        for (int i = beginIndex; i < endIndex; i++) {
+            if (paternStr[i] == '*') {
+                empty = true;
+            } else {
+                if (!empty) {
+                    return false;
+                }
+                empty = false;
+            }
+        }
+        return empty;
+    }
+
+    private boolean match(int strIndex, int paternIndex, char prePaternChar) {
+//        System.out.println("index " + strIndex + " " + paternIndex);
+        if (resultArray[strIndex][paternIndex] != 0) {
+            return resultArray[strIndex][paternIndex] == 1;
+        }
+        boolean result = matchRun(str, strIndex, paternStr, paternIndex, prePaternChar);
+        resultArray[strIndex][paternIndex] = result ? 1 : 2;
+        return result;
+    }
+
+    private boolean matchRun(char[] str, int strIndex, char[] paternStr, int paternIndex, char prePaternChar) {
+        if (strIndex >= str.length) {
+            return isEmptyPatern(paternStr, paternIndex, paternStr.length);
+        }
+        if (paternIndex >= paternStr.length) {
+            return false;
+        }
+        char charIterm = str[strIndex];
+        boolean isEmpty = true;
+        for (int i = paternIndex; i < paternStr.length; i++) {
+            if (paternStr[i] == '*') {
+                if (isEqualsChar(prePaternChar, charIterm)) {
+                    if (match(strIndex + 1, i + 1, prePaternChar)
+                            || match(strIndex + 1, i, prePaternChar)) {
+                        return true;
+                    }
+                }
+                isEmpty = true;
+            } else {
+                if (!isEmpty) {
+                    return false;
+                }
+                prePaternChar = paternStr[i];
+                if (isEqualsChar(paternStr[i], charIterm)) {
+                    if (match(strIndex + 1, i + 1, prePaternChar)) {
+                        return true;
+                    }
+                }
+                isEmpty = false;
+            }
+        }
+        return false;
+    }
+
+    private boolean isEqualsChar(char paternChar, char item) {
+        return paternChar == item || paternChar == '.';
+    }
+}
