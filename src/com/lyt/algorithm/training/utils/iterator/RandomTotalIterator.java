5296e630a541f37304213a209078ea215a34ad3c
kaogu <liangytcomputer@163.com>
2019-11-09 22:58:12 +0800

    有序定和迭代器

---
+++ src/com/lyt/algorithm/training/utils/iterator/RandomTotalIterator.java
@@ -0,0 +1,69 @@
+package com.lyt.algorithm.training.utils.iterator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RandomTotalIterator {
+
+    private final int totalNum;
+    private List<Integer> listNum;
+
+
+    public RandomTotalIterator(int totalNum) {
+        this.totalNum = totalNum;
+    }
+
+    private List<Integer> init(int totalNum) {
+        List<Integer> listNums = new ArrayList<>(totalNum);
+        for (int index = 0; index < totalNum; index++) {
+            listNums.add(1);
+        }
+        return listNums;
+    }
+
+    public static void main(String[] args) {
+        RandomTotalIterator randomTotalIterator = new RandomTotalIterator(30);
+        List<Integer> list = randomTotalIterator.next();
+        int count = 0;
+        while (null != list && !list.isEmpty()) {
+            System.out.println(Arrays.toString(list.toArray()));
+            count++;
+            list = randomTotalIterator.next();
+        }
+        System.out.println("count " + count);
+    }
+
+    public List<Integer> next() {
+        if (null == listNum) {
+            listNum = init(totalNum);
+            return listNum;
+        }
+        if (listNum.isEmpty()) {
+            return listNum;
+        }
+        if (listNum.size() <= 1) {
+            listNum.clear();
+            return listNum;
+        }
+        int sum = listNum.get(listNum.size() - 1);
+        listNum.remove(listNum.size() - 1);
+        for (int index = listNum.size() - 1; index >= 0; index--) {
+            Integer item = listNum.get(index);
+            if (index == 0 || (listNum.get(index) < listNum.get(index - 1))) {
+                listNum.set(index, item + 1);
+                sum--;
+                while (sum > 0) {
+                    listNum.add(1);
+                    sum--;
+                }
+                return listNum;
+            }
+            sum += item;
+            listNum.remove(index);
+        }
+        return null;
+    }
+
+
+}
