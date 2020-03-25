d64a044e995236de53eed3bbb1166eebb8403e44
“KaoGu <liangytcomputer@163.com>
2020-03-25 23:10:21 +0800

    冗余连接2

--- src/com/lyt/algorithm/training/leetcode/redundant_connection_ii/Solution.java
+++ src/com/lyt/algorithm/training/leetcode/redundant_connection_ii/Solution.java
@@ -1,7 +1,82 @@
 package com.lyt.algorithm.training.leetcode.redundant_connection_ii;
 
+import java.util.*;
+
 class Solution {
+    class Uf {
+        int val;
+        Uf parent;
+        List<Uf> sons = new ArrayList<>();
+    }
+
     public int[] findRedundantDirectedConnection(int[][] edges) {
+        Map<Integer, Uf> ufMap = new HashMap<>();
+        Map<String, Integer> edgeIndexMap = new HashMap<>();
+        int[] twoParentUfEdge = null;
+        int index = 0;
+        for (int[] edge : edges) {
+            edgeIndexMap.put(edge[0] + "-" + edge[1], index++);
+            Uf parent = initUfMap(ufMap, edge[0]);
+            Uf son = initUfMap(ufMap, edge[1]);
+            if (son.parent != null) {
+                twoParentUfEdge = edge;
+                continue;
+            }
+            parent.sons.add(son);
+            son.parent = parent;
+        }
+
+        List<Uf> ufs = new ArrayList<>(ufMap.values());
+        if (twoParentUfEdge != null) {
+            if (null == getCircleNode(ufs)) {
+                return twoParentUfEdge;
+            }
+            Uf errorSon = ufMap.get(twoParentUfEdge[1]);
+            return new int[]{errorSon.parent.val, errorSon.val};
+        }
+        Uf circleNode = getCircleNode(ufs);
+        if (null == circleNode) {
+            return null;
+        }
+        Uf parent = circleNode.parent;
+        int maxIndex = edgeIndexMap.get(parent.val + "-" + circleNode.val);
+        while (parent != circleNode) {
+            Integer edgeindex = edgeIndexMap.get(parent.parent.val + "-" + parent.val);
+            if (edgeindex > maxIndex) {
+                maxIndex = edgeindex;
+            }
+            parent = parent.parent;
+        }
+        return edges[maxIndex];
+    }
+
+    private Uf getCircleNode(List<Uf> ufs) {
+        for (Uf uf : ufs) {
+            if (isInCircle(uf, ufs.size())) {
+                return uf;
+            }
+        }
         return null;
+    }
+
+    private boolean isInCircle(Uf uf, int size) {
+        Uf parent = uf.parent;
+        size--;
+        while (size > 0 && parent != null && parent != uf) {
+            parent = parent.parent;
+            size--;
+        }
+        return parent == uf;
+    }
+
+    private Uf initUfMap(Map<Integer, Uf> ufMap, int point) {
+        Uf uf = ufMap.get(point);
+        if (null != uf) {
+            return uf;
+        }
+        uf = new Uf();
+        uf.val = point;
+        ufMap.put(point, uf);
+        return uf;
     }
 }
--- src/com/lyt/algorithm/training/leetcode/redundant_connection_ii/SolutionTest.java
+++ src/com/lyt/algorithm/training/leetcode/redundant_connection_ii/SolutionTest.java
@@ -1,5 +1,7 @@
 package com.lyt.algorithm.training.leetcode.redundant_connection_ii;
 
+import com.lyt.algorithm.training.cbb.list.handler.ArraysCodec;
+import com.lyt.algorithm.training.cbb.list.handler.ListCodec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,25 @@ class SolutionTest {
 
     @Test
     void findRedundantDirectedConnection() {
-        Assertions.assertEquals("","[[1,2], [1,3], [2,3]]");
+        Assertions.assertEquals("", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9]]"))));
+
+        Assertions.assertEquals("[1,5]", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9],[1,5]]"))));
+
+        Assertions.assertEquals("[6,5]", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9],[6,5]]"))));
+
+        Assertions.assertEquals("[3,6]", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9],[3,6]]"))));
+
+        Assertions.assertEquals("[9,5]", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9],[9,5]]"))));
+
+        Assertions.assertEquals("[9,3]", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9],[9,3]]"))));
+
+        Assertions.assertEquals("[8,1]", new ArraysCodec().serializev1(new Solution().findRedundantDirectedConnection(new ArraysCodec().deserialize2v(
+                "[[1,2], [1,3], [1,4], [2,5], [3,7], [4,8], [5,6], [7,9],[8,1]]"))));
     }
 }
